e6891823d8c445fe9883d00260ce67920e2fa538
Sean Dorr <seanmichael.dorr@gmail.com>
2023-04-27 21:57:09 +0200

    feat: creates calendar recipe view

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "../styles/main.scss";
+import dynamic from "next/dynamic";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default dynamic(() => Promise.resolve(App), {
+  ssr: false,
+});
---
+++ src/utils/constants/recipes.ts
@@ -0,0 +1,96 @@
+export const allRecipes = [
+  {
+    name: "Chicken and Bean Taquitos",
+  },
+  {
+    name: "Roasted Veggies & Hummus",
+    units: ["Roasted Veggies", "Hummus"],
+  },
+  {
+    name: "Steak & Mashed Potatoes",
+    units: ["Steak", "Chimichurri", "Mashed Potatoes"],
+  },
+];
+
+export const recipesPerWeek = [
+  {
+    day: "monday",
+    recipes: [
+      {
+        name: "Chicken and Bean Taquitos",
+      },
+      {
+        name: "Roasted Veggies & Hummus",
+        units: ["Roasted Veggies", "Hummus"],
+      },
+      {
+        name: "Steak & Mashed Potatoes",
+        units: ["Steak", "Chimichurri", "Mashed Potatoes"],
+      },
+    ],
+  },
+  {
+    day: "tuesday",
+    recipes: [
+      {
+        name: "Chicken and Bean Taquitos",
+      },
+      {
+        name: "Steak & Mashed Potatoes",
+        units: ["Steak", "Chimichurri", "Mashed Potatoes"],
+      },
+      {
+        name: "Roasted Veggies & Hummus",
+        units: ["Roasted Veggies", "Hummus"],
+      },
+    ],
+  },
+  {
+    day: "wednesday",
+    recipes: [
+      {
+        name: "Roasted Veggies & Hummus",
+        units: ["Roasted Veggies", "Hummus"],
+      },
+      {
+        name: "Chicken and Bean Taquitos",
+      },
+      {
+        name: "Steak & Mashed Potatoes",
+        units: ["Steak", "Chimichurri", "Mashed Potatoes"],
+      },
+    ],
+  },
+  {
+    day: "thursday",
+    recipes: [
+      {
+        name: "Chicken and Bean Taquitos",
+      },
+      {
+        name: "Roasted Veggies & Hummus",
+        units: ["Roasted Veggies", "Hummus"],
+      },
+      {
+        name: "Steak & Mashed Potatoes",
+        units: ["Steak", "Chimichurri", "Mashed Potatoes"],
+      },
+    ],
+  },
+  {
+    day: "friday",
+    recipes: [
+      {
+        name: "Chicken and Bean Taquitos",
+      },
+      {
+        name: "Roasted Veggies & Hummus",
+        units: ["Roasted Veggies", "Hummus"],
+      },
+      {
+        name: "Steak & Mashed Potatoes",
+        units: ["Steak", "Chimichurri", "Mashed Potatoes"],
+      },
+    ],
+  },
+];
